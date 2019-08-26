bcc06f21d1bfd484548a7ab63a19c2c144576ea7
Vitor Bryan <45441601+vBryann@users.noreply.github.com>
2019-08-26 10:10:45 -0300

    Create counting.py

---
+++ countingsort/counting.py
@@ -0,0 +1,52 @@
+import timeit
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from random import shuffle
+
+mpl.use('Agg')
+
+def listaInvertida(tam):
+    lista = list(range(1, tam + 1))
+    return lista[::-1]
+
+def listaAleatoria(tam):
+    lista = list(range(1, tam + 1))
+    shuffle(lista)
+    return lista
+
+
+def count_sort(tam):
+    pivo = max(tam)
+    aux = [0] * (pivo + 1)
+    for i in range(len(tam)):
+        aux[tam[i]] += 1
+    x = 0
+    for i in range(len(aux)):
+        aux1 = aux[i]
+        for _ in range(aux1):
+            tam[x] = i
+            x += 1
+
+def desenhaGrafico(x, y,label, name, xl="Tamanho da Lista", yl="Tempo"):
+
+    fig = plt.figure(figsize=(10, 8))
+    ax = fig.add_subplot(111)
+    ax.plot(x, y, label=label )
+    ax.legend(bbox_to_anchor=(1, 1), bbox_transform=plt.gcf().transFigure)
+    plt.ylabel(yl)
+    plt.xlabel(xl)
+    fig.savefig(name)
+
+
+tam = [100000, 200000, 400000, 500000, 1000000, 2000000]
+time = []
+
+for i in range(len(tam)):
+    lista = listaAleatoria(tam[i])
+    time.append(
+        timeit.timeit("count_sort({})".format(lista), setup="from __main__ import count_sort", number=1))
+
+    print("executado o sort na lista de tamanho", tam[i])
+
+
+desenhaGrafico(tam, time,"Lista aleatória", "tempo.png")
